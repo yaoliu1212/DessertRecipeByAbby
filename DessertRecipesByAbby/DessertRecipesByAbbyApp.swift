72f14690c3855b29a07da6d883dfbf9705d7428c
Yao Liu <yliu204@u.rochester.edu>
2024-07-20 19:05:33 -0400

    Initial Commit

---
+++ DessertRecipesByAbby/DessertRecipesByAbbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesByAbbyApp.swift
+//  DessertRecipesByAbby
+//
+//  Created by Yao Liu on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesByAbbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
